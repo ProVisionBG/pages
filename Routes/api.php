1daf965c1adc847b45486d9dda0988c56e47eeff
Venelin Iliev <venelin@provision.bg>
2016-12-10 15:15:18 +0200

    custom install path

--- Routes/api.php
+++ Routes/api.php
@@ -12,7 +12,3 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-//Route::get('/pages', function (Request $request) {
-//    // return $request->pages();
-//})->middleware('auth:api');
